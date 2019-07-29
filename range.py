325508c1eb7d226dbb6679b051790179f55901dd
Amruta Patil <patilamruta299@gmail.com>
2019-07-29 17:41:35 +0530

    whether number is in given range

---
+++ range.py
@@ -0,0 +1,7 @@
+def find_range():
+    x=int(input('enter the number'))
+    if x in range(1,11):
+        print('number is in range')
+    else:
+         print('number is not in range') 
+find_range()           
