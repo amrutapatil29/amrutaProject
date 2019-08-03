6adf9e53b45b4ce13510bcfce0d1545a19128d34
Amruta Patil <patilamruta299@gmail.com>
2019-08-03 18:19:14 +0530

    prime number

--- prime.py
+++ prime.py
@@ -1,12 +1,11 @@
 n=int(input('enter the number'))
 if n>1:
-    for i in range(2,n):
+    for i in range(2,n+1):
         if n%i==0:
-            print('number is not prime number')
             break
-        else:
-            print('number is prime number')
-            break
-else:            
-    print('number is not prime')        
-        
+    if n==i:
+        print('number is prime number')
+    else:
+        print('number is not prime number')
+else:             
+     print('number is not prime number')
